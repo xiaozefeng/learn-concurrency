77f098097b06a2e9590b44325bde4cb0d50da836
肖泽锋 <qq523107430@163.com>
2018-04-22 23:43:58 +0800

    并发容器-安全发布对象总结

---
+++ src/main/java/org/dark/concurrency/example/concurrent/CopyOnWriteArraySetExample.java
@@ -0,0 +1,54 @@
+package org.dark.concurrency.example.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dark.concurrency.annotations.ThreadSafe;
+
+import java.util.Set;
+import java.util.concurrent.*;
+
+/**
+ * 并发容器  CopyOnWriteArraySet
+ *
+ * @author xiaozefeng
+ * @date 2018/4/22 下午11:24
+ */
+@Slf4j
+@ThreadSafe
+public class CopyOnWriteArraySetExample {
+    private static int clientTotal = 5000;
+    private static int threadTotal = 200;
+
+    private static Set<Integer> list = new CopyOnWriteArraySet<>();
+
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        final Semaphore semaphore = new Semaphore(threadTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            final int temp = i;
+            executorService.submit(() -> {
+                try {
+                    semaphore.acquire();
+                    update(temp);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error(e.getMessage(), e);
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+
+        log.info("list size :{}",list.size());
+    }
+
+    private static void update(int temp) {
+        list.add(temp);
+    }
+
+}
